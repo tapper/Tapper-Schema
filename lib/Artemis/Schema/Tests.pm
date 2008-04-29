e2a3e3d22bd8afa80bd69109a34d0c7a1c96bde5
Steffen Schwigon <steffen.schwigon@amd.com>
2008-04-29 17:48:59 +0200

    - Moose'ified NetLogAppender   * this helps to avoid inventing own OO mechanisms - initial DBIx::Class connection stuff - (yet unsuccessful) use of SQL::Translator - optional export/import of subs (EXPORT_OK)

--- lib/Artemis/Schema/Tests.pm
+++ lib/Artemis/Schema/Tests.pm
@@ -7,6 +7,8 @@ use base 'DBIx::Class::Schema';
 
 __PACKAGE__->load_classes;
 
+__PACKAGE__->connection([ "dbi:mysql:dbname=testsystem;host=alzey", "root", "xyzxyzaa" ]);
+
 
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-04-28 17:22:18
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:KPn7WC57JfwYQ0uj5H3b/A
