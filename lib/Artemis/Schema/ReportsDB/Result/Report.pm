b5e7f560430afb6a2ae39926c1d5679b709ea38e
Steffen Schwigon <steffen.schwigon@amd.com>
2008-06-19 17:51:57 +0200

    - now per-section meta info possible

--- lib/Artemis/Schema/ReportsDB/Result/Report.pm
+++ lib/Artemis/Schema/ReportsDB/Result/Report.pm
@@ -10,65 +10,51 @@ __PACKAGE__->load_components(qw(InflateColumn::DateTime TimeStamp Core));
 __PACKAGE__->table("report");
 __PACKAGE__->add_columns
     (
-     "id",                      { data_type => "INT",      default_value => undef,  is_nullable => 0, size => 11, is_auto_increment => 1, },
-     "suite_id",                { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 11, is_foreign_key => 1,    },
-     "suite_version",           { data_type => "VARCHAR",  default_value => undef,  is_nullable => 1, size => 11,                         },
+     "id",                      { data_type => "INT",      default_value => undef,  is_nullable => 0, size => 11, is_auto_increment => 1,     },
+     "suite_id",                { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 11, is_foreign_key => 1,        },
+     "suite_version",           { data_type => "VARCHAR",  default_value => undef,  is_nullable => 1, size => 11,                             },
+     "reportername",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 100,                            },
      #
-     "tap",                     { data_type => "TEXT",     default_value => "",     is_nullable => 0, size => 65535,                      },
-     "successgrade",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                         },
-     "reviewed_successgrade",   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                         },
+     # raw tap
      #
+     "tap",                     { data_type => "TEXT",     default_value => "",     is_nullable => 0, size => 65535,                          },
      #
-     "total",                   { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "failed",                  { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "parse_errors",            { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "passed",                  { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "skipped",                 { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "todo",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "todo_passed",             { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "wait",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "exit",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                         },
-     "success_ratio",           { data_type => "VARCHAR",  default_value => undef,  is_nullable => 1, size => 20,                         },
-
+     # tap parse result and its human interpretation
      #
-     "starttime_test_program",  { data_type => "DATETIME", default_value => undef,  is_nullable => 1,                                     },
-     "endtime_test_program",    { data_type => "DATETIME", default_value => undef,  is_nullable => 1,                                     },
+     "successgrade",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                             },
+     "reviewed_successgrade",   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                             },
      #
+     # tap parse results
      #
-     "machine_name",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 50,                         },
-     "machine_description",     { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
+     "total",                   { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "failed",                  { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "parse_errors",            { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "passed",                  { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "skipped",                 { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "todo",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "todo_passed",             { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "wait",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "exit",                    { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                             },
+     "success_ratio",           { data_type => "VARCHAR",  default_value => undef,  is_nullable => 1, size => 20,                             },
      #
-     "created_at",              { data_type => "DATETIME", default_value => undef,  is_nullable => 0, set_on_create => 1,                 },
+     "starttime_test_program",  { data_type => "DATETIME", default_value => undef,  is_nullable => 1,                                         },
+     "endtime_test_program",    { data_type => "DATETIME", default_value => undef,  is_nullable => 1,                                         },
+     #
+     "machine_name",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 50,                             },
+     "machine_description",     { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                          },
+     #
+     "created_at",              { data_type => "DATETIME", default_value => undef,  is_nullable => 0, set_on_create => 1,                     },
      "updated_at",              { data_type => "DATETIME", default_value => undef,  is_nullable => 0, set_on_create => 1, set_on_update => 1, },
-     "reportername",            { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 100,                        },
-     #
-     # TODO: these all get into ReportSection
-     "osname",                  { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                        },
-     "uname",                   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                        },
-     "language_description",    { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 255,                        },
-     "cpuinfo",                 { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "ram",                     { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 50,                         },
-     "lspci",                   { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "lsusb",                   { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "flags",                   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                        },
-     "xen_changeset",           { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                        },
-     "xen_hvbits",              { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                         },
-     "xen_dom0_kernel",         { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "xen_base_os_description", { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "xen_guests_description",  { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                      },
-     "test_was_on_guest",       { data_type => "INT",      default_value => 0,      is_nullable => 1, size => 1,                          },
-     "test_was_on_hv",          { data_type => "INT",      default_value => 0,      is_nullable => 1, size => 1,                          },
     );
 
 __PACKAGE__->set_primary_key("id");
 
-__PACKAGE__->belongs_to   ( suite    => 'Artemis::Schema::ReportsDB::Result::Suite',         { 'foreign.id'        => 'self.suite_id' });
+__PACKAGE__->belongs_to   ( suite          => 'Artemis::Schema::ReportsDB::Result::Suite',         { 'foreign.id'        => 'self.suite_id' });
 
 __PACKAGE__->has_many     ( comments       => 'Artemis::Schema::ReportsDB::Result::ReportComment', { 'foreign.report_id' => 'self.id' });
 __PACKAGE__->has_many     ( topics         => 'Artemis::Schema::ReportsDB::Result::ReportTopic',   { 'foreign.report_id' => 'self.id' });
 __PACKAGE__->has_many     ( files          => 'Artemis::Schema::ReportsDB::Result::ReportFile',    { 'foreign.report_id' => 'self.id' });
 __PACKAGE__->has_many     ( reportsections => 'Artemis::Schema::ReportsDB::Result::ReportSection', { 'foreign.report_id' => 'self.id' });
-
 
 1;
 
--- lib/Artemis/Schema/ReportsDB/Result/ReportSection.pm
+++ lib/Artemis/Schema/ReportsDB/Result/ReportSection.pm
@@ -6,16 +6,33 @@ use warnings;
 use parent 'DBIx::Class';
 use parent 'Artemis::Schema::Printable';
 
-# TODO: 
 __PACKAGE__->load_components("Core");
 __PACKAGE__->table("reportsection");
 __PACKAGE__->add_columns
     (
-     "report_id",       { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key => 1, },
-     "succession",      { data_type => "INT", default_value => undef, is_nullable => 1, size => 10,                      },
+     "id",                      { data_type => "INT",      default_value => undef,  is_nullable => 0, size => 11, is_auto_increment => 1, },
+     "report_id",               { data_type => "INT",      default_value => undef,  is_nullable => 0, size => 11, is_foreign_key => 1, },
+     "succession",              { data_type => "INT",      default_value => undef,  is_nullable => 1, size => 10,                      },
+     "name",                    { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                     },
+     #
+     "osname",                  { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                     },
+     "uname",                   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                     },
+     "language_description",    { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 255,                     },
+     "cpuinfo",                 { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "ram",                     { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 50,                      },
+     "lspci",                   { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "lsusb",                   { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "flags",                   { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                     },
+     "xen_changeset",           { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 255,                     },
+     "xen_hvbits",              { data_type => "VARCHAR",  default_value => "",     is_nullable => 1, size => 10,                      },
+     "xen_dom0_kernel",         { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "xen_base_os_description", { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "xen_guests_description",  { data_type => "TEXT",     default_value => "",     is_nullable => 1, size => 65535,                   },
+     "test_was_on_guest",       { data_type => "INT",      default_value => 0,      is_nullable => 1, size => 1,                       },
+     "test_was_on_hv",          { data_type => "INT",      default_value => 0,      is_nullable => 1, size => 1,                       },
     );
 
-__PACKAGE__->set_primary_key("report_id");
+__PACKAGE__->set_primary_key("id");
 
 __PACKAGE__->has_many   ( report => 'Artemis::Schema::ReportsDB::Result::Report', { 'foreign.id' => 'self.report_id' });
 
