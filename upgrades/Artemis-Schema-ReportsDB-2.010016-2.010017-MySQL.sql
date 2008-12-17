f341d3eeec82ce32e23863cd6ff891687e694a7e
Steffen Schwigon <steffen.schwigon@amd.com>
2008-12-17 12:40:22 +0100

    - DB upgrade scripts with peer info

---
+++ upgrades/Artemis-Schema-ReportsDB-2.010016-2.010017-MySQL.sql
@@ -0,0 +1,10 @@
+-- Convert schema 'upgrades/Artemis-Schema-ReportsDB-2.010016-MySQL.sql' to 'Artemis::Schema::ReportsDB v2.010017':
+
+BEGIN;
+
+ALTER TABLE report ADD COLUMN peeraddr VARCHAR(20) DEFAULT '',
+                   ADD COLUMN peerport VARCHAR(20) DEFAULT '',
+                   ADD COLUMN peerhost VARCHAR(255) DEFAULT '';
+ALTER TABLE reportsection CHANGE COLUMN language_description language_description text;
+
+COMMIT;
---
+++ upgrades/Artemis-Schema-ReportsDB-2.010016-2.010017-SQLite.sql
@@ -0,0 +1,18 @@
+-- Convert schema 'upgrades/Artemis-Schema-ReportsDB-2.010016-SQLite.sql' to 'upgrades/Artemis-Schema-ReportsDB-2.010017-SQLite.sql':
+
+BEGIN;
+
+ALTER TABLE report ADD COLUMN peeraddr VARCHAR(20) DEFAULT '';
+ALTER TABLE report ADD COLUMN peerport VARCHAR(20) DEFAULT '';
+ALTER TABLE report ADD COLUMN peerhost VARCHAR(255) DEFAULT '';
+
+
+
+
+
+
+
+
+
+
+COMMIT;
