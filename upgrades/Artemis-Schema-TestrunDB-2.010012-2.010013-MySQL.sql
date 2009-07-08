07a6746532d97d1303ad8b584d9a436eb00376a1
Steffen Schwigon <steffen.schwigon@amd.com>
2009-07-08 11:09:35 +0200

    Schema: testrundb with timestamps

---
+++ upgrades/Artemis-Schema-TestrunDB-2.010012-2.010013-MySQL.sql
@@ -0,0 +1,9 @@
+-- Convert schema 'upgrades/Artemis-Schema-TestrunDB-2.010012-MySQL.sql' to 'Artemis::Schema::TestrunDB v2.010013':;
+
+BEGIN;
+
+ALTER TABLE testrun CHANGE COLUMN created_at created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP';
+
+
+COMMIT;
+
