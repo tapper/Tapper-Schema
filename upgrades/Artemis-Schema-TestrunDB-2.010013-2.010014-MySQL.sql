78acd68a3af113da798014388901694ce086a260
Steffen Schwigon <steffen.schwigon@amd.com>
2009-07-08 11:37:51 +0200

    Schema: bump schema version to correctly handle upgrades

---
+++ upgrades/Artemis-Schema-TestrunDB-2.010013-2.010014-MySQL.sql
@@ -0,0 +1,9 @@
+-- Convert schema 'upgrades/Artemis-Schema-TestrunDB-2.010013-MySQL.sql' to 'Artemis::Schema::TestrunDB v2.010014':;
+
+BEGIN;
+
+ALTER TABLE testrun CHANGE COLUMN created_at created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+
+COMMIT;
+
