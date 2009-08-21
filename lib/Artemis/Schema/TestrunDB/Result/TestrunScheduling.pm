73d76bf7b194605fa2475d14caed4cf2d5773c99
Steffen Schwigon <steffen.schwigon@amd.com>
2009-08-21 17:06:10 +0200

    Scheduler/Schema: add "built" flag for build scheduling state

--- lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
+++ lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
@@ -12,6 +12,7 @@ __PACKAGE__->add_columns
      "id",              { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id",      { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key => 1,    },
      "queue_id",        { data_type => "INT", default_value => 0,     is_nullable => 1, size => 11, is_foreign_key => 1,    },
+     "built",           { data_type => "INT", default_value => 0,     is_nullable => 1, size => 1,                          },
      "active",          { data_type => "INT", default_value => 0,     is_nullable => 1, size => 1,                          },
      "created_at",      { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                        }, # '
      "updated_at",      { data_type => "DATETIME",  default_value => undef,                is_nullable => 1,                                        },
