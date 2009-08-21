1574290097cec7dadb7d0a89d4d38699a39594c4
Steffen Schwigon <steffen.schwigon@amd.com>
2009-08-21 10:20:15 +0200

    Scheduler/Schema: add tables for scheduling

---
+++ lib/Artemis/Schema/TestrunDB/Result/Queue.pm
@@ -0,0 +1,117 @@
+package Artemis::Schema::TestrunDB::Result::Queue;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class';
+
+__PACKAGE__->load_components(qw/InflateColumn::DateTime Core/);
+__PACKAGE__->table("queue");
+__PACKAGE__->add_columns
+    (
+     "id",                        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11,    is_auto_increment => 1, },
+     "name",                      { data_type => "VARCHAR",   default_value => "",                   is_nullable => 1, size => 255,                           },
+     "producer",                  { data_type => "VARCHAR",   default_value => "",                   is_nullable => 1, size => 255,                           },
+     "priority",                  { data_type => "INT",       default_value => 0,                    is_nullable => 0, size => 10,                            },
+     "runcount",                  { data_type => "INT",       default_value => 0,                    is_nullable => 0, size => 10,                            },
+     "created_at",                { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                        }, # '
+     "updated_at",                { data_type => "DATETIME",  default_value => undef,                is_nullable => 1,                                        },
+    );
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->has_many ( testrunschedulings => 'Artemis::Schema::TestrunDB::Result::TestrunScheduling', { 'foreign.queue_id' => 'self.id' });
+
+# -------------------- methods on results --------------------
+
+sub to_string
+{
+        my ($self) = @_;
+
+        my $format = join( $Artemis::Schema::TestrunDB::DELIM, qw/%s %s %s %s %s %s %s %s %s %s %s %s %s %s /, '');
+        sprintf (
+                 $format,
+                 map {
+                      defined $self->$_
+                      ? $self->$_
+                      : $Artemis::Schema::TestrunDB::NULL
+                     } @{$self->result_source->{_ordered_columns} }
+                );
+}
+
+=head2 is_member($head, @tail)
+
+Checks if the first element is already in the list of the remaining
+elements.
+
+=cut
+
+sub is_member
+{
+        my ($head, @tail) = @_;
+        grep { $head->id eq $_->id } @tail;
+}
+
+=head2 ordered_preconditions
+
+Returns all preconditions in the order they need to be installed.
+
+=cut
+
+sub ordered_preconditions
+{
+        my ($self) = @_;
+
+        my @done = ();
+        my %seen = ();
+        my @todo = ();
+
+        @todo = $self->preconditions->search({}, {order_by => 'succession'})->all;
+
+        while (my $head = shift @todo)
+        {
+                if ($seen{$head->id})
+                {
+                        push @done, $head unless is_member($head, @done);
+                }
+                else
+                {
+                        $seen{$head->id} = 1;
+                        my @pre_todo = $head->child_preconditions->search({}, { order_by => 'succession' } )->all;
+                        unshift @todo, @pre_todo, $head;
+                }
+        }
+        return @done;
+}
+
+
+1;
+
+=head1 NAME
+
+Artemis::Schema::TestrunDB::Testrun - A ResultSet description
+
+
+=head1 SYNOPSIS
+
+Abstraction for the database table.
+
+ use Artemis::Schema::TestrunDB;
+
+
+=head1 AUTHOR
+
+OSRC SysInt Team, C<< <osrc-sysint at elbe.amd.com> >>
+
+
+=head1 BUGS
+
+None.
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 OSRC SysInt Team, all rights reserved.
+
+This program is released under the following license: restrictive
+
---
+++ lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
@@ -0,0 +1,56 @@
+package Artemis::Schema::TestrunDB::Result::TestrunScheduling;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class';
+
+__PACKAGE__->load_components("Core");
+__PACKAGE__->table("testrun_scheduling");
+__PACKAGE__->add_columns
+    (
+     "id",              { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
+     "testrun_id",      { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key => 1,    },
+     "queue_id",        { data_type => "INT", default_value => 0,     is_nullable => 1, size => 11, is_foreign_key => 1,    },
+     "active",          { data_type => "INT", default_value => 0,     is_nullable => 1, size => 1,                          },
+     "created_at",      { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                        }, # '
+     "updated_at",      { data_type => "DATETIME",  default_value => undef,                is_nullable => 1,                                        },
+    );
+
+__PACKAGE__->set_primary_key(qw/testrun_id/);
+
+__PACKAGE__->belongs_to( testrun            => 'Artemis::Schema::TestrunDB::Result::Testrun',                 { 'foreign.id'         => 'self.testrun_id' });
+__PACKAGE__->belongs_to( queue              => 'Artemis::Schema::TestrunDB::Result::Testrun',                 { 'foreign.id'         => 'self.queue_id'   });
+
+__PACKAGE__->has_many  ( requested_features => 'Artemis::Schema::TestrunDB::Result::TestrunRequestedFeature', { 'foreign.testrun_id' => 'self.id'         });
+
+1;
+
+=head1 NAME
+
+Artemis::Schema::TestrunDB::Result::PrePrecondition - A ResultSet description
+
+
+=head1 SYNOPSIS
+
+Abstraction for the database table.
+
+ use Artemis::Schema::TestrunDB;
+
+
+=head1 AUTHOR
+
+OSRC SysInt Team, C<< <osrc-sysint at elbe.amd.com> >>
+
+
+=head1 BUGS
+
+None.
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 OSRC SysInt Team, all rights reserved.
+
+This program is released under the following license: restrictive
+
