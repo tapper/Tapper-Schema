a585cd4c3d13ab9abbac1098d9ed45ffd0659e62
Maik Hentsche <maik.hentsche@amd.com>
2009-08-26 10:39:42 +0200

    Schema: fixed name of has_many relation in systems to match its plural nature

--- lib/Artemis/Schema/HardwareDB/Result/Systems.pm
+++ lib/Artemis/Schema/HardwareDB/Result/Systems.pm
@@ -66,7 +66,7 @@ __PACKAGE__->set_primary_key("lid");
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-05-22 14:22:46
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:8LO4b+LChpwxSvGCeRAu7g
 
-__PACKAGE__->has_many ( revision => 'Artemis::Schema::HardwareDB::Result::Revision', { 'foreign.revision_id' => 'self.revision_id' });
+__PACKAGE__->has_many ( revisions => 'Artemis::Schema::HardwareDB::Result::Revision', { 'foreign.revision_id' => 'self.revision_id' });
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
