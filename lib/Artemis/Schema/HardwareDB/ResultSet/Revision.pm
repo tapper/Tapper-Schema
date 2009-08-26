9a1ca6855c05ea1ce9003af8b34c2b466b450aad
Maik Hentsche <maik.hentsche@amd.com>
2009-08-26 10:40:53 +0200

    Schema: added component accessors for system revisions

--- lib/Artemis/Schema/HardwareDB/ResultSet/Revision.pm
+++ lib/Artemis/Schema/HardwareDB/ResultSet/Revision.pm
@@ -9,17 +9,27 @@ use DateTime;
 use parent 'DBIx::Class::ResultSet';
 
 
-___END___
+# (XXX) do it with joins
+sub cpus { shift->_component('cpu') }
+sub rams { shift->_component('ram') }
 
-# just an idea on how to implement this, don't want to throw it away
-
-sub cpu_array
-{
-        my ($self) = @_;
-        my $cpu_revision_rs = $self->search({component_type => 'cpu'});
-        my @resultset;
-        foreach my $cpu ($cpu_revision_rs->next) {
-                push @resultset, $self->result_source->schema->resultset('Cpu')->find($cpu->component_id);
-        }
-        return @resultset;
+sub mem {
+        my $size = 0;
+        $size   += $_ foreach map {$_->size_mbyte} shift->rams;
+        return $size;
 }
+
+sub _component
+{
+        my ($self, $type) = @_;
+
+        my $revision_rs = $self->search({component_type => $type});
+        my @component_ids;
+        while ( my $component = $revision_rs->next) {
+                push @component_ids, {lid => $component->component_id};
+        }
+        my $search_params = { -or => \@component_ids };
+        return $self->result_source->schema->resultset(ucfirst($type))->search( $search_params);
+}
+
+1;
