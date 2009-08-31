4c0f8632ad24dda6f2b266de005ed45a23efe1ad
Steffen Schwigon <steffen.schwigon@amd.com>
2009-08-31 11:19:37 +0200

    Schema: host <--> scheduling relations

--- lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
+++ lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
@@ -12,6 +12,7 @@ __PACKAGE__->add_columns
      "id",              { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id",      { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key => 1,    },
      "queue_id",        { data_type => "INT", default_value => 0,     is_nullable => 1, size => 11, is_foreign_key => 1,    },
+     "host_id",         { data_type => "INT", default_value => 0,     is_nullable => 1, size => 11, is_foreign_key => 1,    },
      "built",           { data_type => "INT", default_value => 0,     is_nullable => 1, size => 1,                          },
      "active",          { data_type => "INT", default_value => 0,     is_nullable => 1, size => 1,                          },
      "created_at",      { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                        }, # '
@@ -22,6 +23,7 @@ __PACKAGE__->set_primary_key(qw/testrun_id/);
 
 __PACKAGE__->belongs_to( testrun            => 'Artemis::Schema::TestrunDB::Result::Testrun',                 { 'foreign.id'         => 'self.testrun_id' });
 __PACKAGE__->belongs_to( queue              => 'Artemis::Schema::TestrunDB::Result::Queue',                   { 'foreign.id'         => 'self.queue_id'   });
+__PACKAGE__->belongs_to( host               => 'Artemis::Schema::TestrunDB::Result::Host',                    { 'foreign.id'         => 'self.host_id'    });
 
 __PACKAGE__->has_many  ( requested_features => 'Artemis::Schema::TestrunDB::Result::TestrunRequestedFeature', { 'foreign.testrun_id' => 'self.id'         });
 
