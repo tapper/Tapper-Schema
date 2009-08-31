73708543e69991de25b6043a53956358652bd37f
Steffen Schwigon <steffen.schwigon@amd.com>
2009-08-31 10:10:51 +0200

    Schema: cherry-picked (schön wär's) schema fixes from scheduler branch

--- lib/Artemis/Schema/TestrunDB/Result/TestrunRequestedFeature.pm
+++ lib/Artemis/Schema/TestrunDB/Result/TestrunRequestedFeature.pm
@@ -10,13 +10,13 @@ __PACKAGE__->table("testrun_requested_feature");
 __PACKAGE__->add_columns
     (
      "id",              { data_type => "INT",     default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
-     "testrun_id",      { data_type => "INT",     default_value => undef, is_nullable => 0, size => 11, is_foreign_key => 1,    },
+     "testrun_id",      { data_type => "INT",     default_value => undef, is_nullable => 0, size => 11, is_foreign_key    => 1, },
      "feature",         { data_type => "VARCHAR", default_value => "",    is_nullable => 1, size => 255,                        },
     );
 
-__PACKAGE__->set_primary_key(qw/testrun_id/);
+__PACKAGE__->set_primary_key(qw/id/);
 
-__PACKAGE__->belongs_to( testrun            => 'Artemis::Schema::TestrunDB::Result::Testrun',                 { 'foreign.id'         => 'self.testrun_id' });
+__PACKAGE__->belongs_to( testrun => 'Artemis::Schema::TestrunDB::Result::Testrun', { 'foreign.id' => 'self.testrun_id' });
 
 1;
 
