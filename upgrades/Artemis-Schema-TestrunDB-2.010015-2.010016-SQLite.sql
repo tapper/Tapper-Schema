58b63d5b0c07aaa83546ee20487816dcece32dfb
Maik Hentsche <maik.hentsche@amd.com>
2009-09-01 12:51:47 +0200

    Schema: fixed scalar refs in 2.010016 schemas

--- upgrades/Artemis-Schema-TestrunDB-2.010015-2.010016-SQLite.sql
+++ upgrades/Artemis-Schema-TestrunDB-2.010015-2.010016-SQLite.sql
@@ -1,9 +1,6 @@
--- Convert schema 'upgrades/Artemis-Schema-TestrunDB-2.010015-SQLite.sql' to 'upgrades/Artemis-Schema-TestrunDB-2.010016-SQLite.sql':
+-- Convert schema 'upgrades/Artemis-Schema-TestrunDB-2.010015-SQLite.sql' to 'upgrades/Artemis-Schema-TestrunDB-2.010016-SQLite.sql':;
 
 BEGIN;
-
-
-
 
 CREATE TEMPORARY TABLE queue_temp_alter (
   id INTEGER PRIMARY KEY NOT NULL,
@@ -11,21 +8,26 @@ CREATE TEMPORARY TABLE queue_temp_alter (
   producer VARCHAR(255) DEFAULT '',
   priority INT(10) NOT NULL DEFAULT '0',
   runcount INT(10) NOT NULL DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1416970)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
+
 INSERT INTO queue_temp_alter SELECT id, name, producer, priority, runcount, created_at, updated_at FROM queue;
+
 DROP TABLE queue;
+
 CREATE TABLE queue (
   id INTEGER PRIMARY KEY NOT NULL,
   name VARCHAR(255) DEFAULT '',
   producer VARCHAR(255) DEFAULT '',
   priority INT(10) NOT NULL DEFAULT '0',
   runcount INT(10) NOT NULL DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1416970)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
+
 INSERT INTO queue SELECT id, name, producer, priority, runcount, created_at, updated_at FROM queue_temp_alter;
+
 DROP TABLE queue_temp_alter;
 
 CREATE TEMPORARY TABLE testrun_temp_alter (
@@ -41,11 +43,14 @@ CREATE TEMPORARY TABLE testrun_temp_alter (
   owner_user_id INT(11),
   test_program VARCHAR(255) NOT NULL DEFAULT '',
   wait_after_tests INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1438370)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
+
 INSERT INTO testrun_temp_alter SELECT id, shortname, notes, topic_name, starttime_earliest, starttime_testrun, starttime_test_program, endtime_test_program, hardwaredb_systems_id, owner_user_id, test_program, wait_after_tests, created_at, updated_at FROM testrun;
+
 DROP TABLE testrun;
+
 CREATE TABLE testrun (
   id INTEGER PRIMARY KEY NOT NULL,
   shortname VARCHAR(255) DEFAULT '',
@@ -59,29 +64,38 @@ CREATE TABLE testrun (
   owner_user_id INT(11),
   test_program VARCHAR(255) NOT NULL DEFAULT '',
   wait_after_tests INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1438370)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
-CREATE INDEX testrun_idx_owner_user_id_test_testru ON testrun (owner_user_id);
-CREATE INDEX testrun_idx_topic_name_testrun_testru ON testrun (topic_name);
-INSERT INTO testrun SELECT id, shortname, notes, topic_name, starttime_earliest, starttime_testrun, starttime_test_program, endtime_test_program, hardwaredb_systems_id, owner_user_id, test_program, wait_after_tests, created_at, updated_at FROM testrun_temp_alter;
-DROP TABLE testrun_temp_alter;
 
+CREATE INDEX testrun_idx_owner_user_id03 ON testrun (owner_user_id);
+
+CREATE INDEX testrun_idx_topic_name03 ON testrun (topic_name);
+
+INSERT INTO testrun SELECT id, shortname, notes, topic_name, starttime_earliest, starttime_testrun, starttime_test_program, endtime_test_program, hardwaredb_systems_id, owner_user_id, test_program, wait_after_tests, created_at, updated_at FROM testrun_temp_alter;
+
+DROP TABLE testrun_temp_alter;
 
 CREATE TEMPORARY TABLE testrun_requested_feature_temp_alter (
   id INTEGER PRIMARY KEY NOT NULL,
   testrun_id INT(11) NOT NULL,
   feature VARCHAR(255) DEFAULT ''
 );
+
 INSERT INTO testrun_requested_feature_temp_alter SELECT id, testrun_id, feature FROM testrun_requested_feature;
+
 DROP TABLE testrun_requested_feature;
+
 CREATE TABLE testrun_requested_feature (
   id INTEGER PRIMARY KEY NOT NULL,
   testrun_id INT(11) NOT NULL,
   feature VARCHAR(255) DEFAULT ''
 );
-CREATE INDEX testrun_requested_feature_idx_testrun_id_testrun_reques_ ON testrun_requested_feature (testrun_id);
+
+CREATE INDEX testrun_requested_feature_i00 ON testrun_requested_feature (testrun_id);
+
 INSERT INTO testrun_requested_feature SELECT id, testrun_id, feature FROM testrun_requested_feature_temp_alter;
+
 DROP TABLE testrun_requested_feature_temp_alter;
 
 CREATE TEMPORARY TABLE testrun_scheduling_temp_alter (
@@ -90,26 +104,32 @@ CREATE TEMPORARY TABLE testrun_scheduling_temp_alter (
   queue_id INT(11) DEFAULT '0',
   built INT(1) DEFAULT '0',
   active INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1392860)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
+
 INSERT INTO testrun_scheduling_temp_alter SELECT id, testrun_id, queue_id, built, active, created_at, updated_at FROM testrun_scheduling;
+
 DROP TABLE testrun_scheduling;
+
 CREATE TABLE testrun_scheduling (
   id INT NOT NULL,
   testrun_id INTEGER PRIMARY KEY NOT NULL,
   queue_id INT(11) DEFAULT '0',
   built INT(1) DEFAULT '0',
   active INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1392860)',
+  created_at TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP',
   updated_at DATETIME
 );
-CREATE INDEX testrun_scheduling_idx_queue_id_testrun_scheduli_ ON testrun_scheduling (queue_id);
-CREATE INDEX testrun_scheduling_idx_testrun_id_testrun_schedu_ ON testrun_scheduling (testrun_id);
+
+CREATE INDEX testrun_scheduling_idx_queu00 ON testrun_scheduling (queue_id);
+
+CREATE INDEX testrun_scheduling_idx_test00 ON testrun_scheduling (testrun_id);
+
 INSERT INTO testrun_scheduling SELECT id, testrun_id, queue_id, built, active, created_at, updated_at FROM testrun_scheduling_temp_alter;
+
 DROP TABLE testrun_scheduling_temp_alter;
 
 
-
-
 COMMIT;
+
--- upgrades/Artemis-Schema-TestrunDB-2.010016-SQLite.sql
+++ upgrades/Artemis-Schema-TestrunDB-2.010016-SQLite.sql
@@ -1,14 +1,16 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Mon Aug 31 10:37:28 2009
+-- Created on Mon Aug 31 13:25:13 2009
 -- 
-BEGIN TRANSACTION;
 
+
+BEGIN TRANSACTION;
 
 --
 -- Table: precondition
 --
 DROP TABLE precondition;
+
 CREATE TABLE precondition (
   id INTEGER PRIMARY KEY NOT NULL,
   shortname VARCHAR(255) NOT NULL DEFAULT '',
@@ -16,48 +18,80 @@ CREATE TABLE precondition (
   timeout INT(10)
 );
 
-
---
--- Table: preconditiontype
---
-DROP TABLE preconditiontype;
-CREATE TABLE preconditiontype (
-  name VARCHAR(20) NOT NULL,
-  description TEXT NOT NULL DEFAULT '',
-  PRIMARY KEY (name)
-);
-
-
 --
 -- Table: queue
 --
 DROP TABLE queue;
+
 CREATE TABLE queue (
   id INTEGER PRIMARY KEY NOT NULL,
   name VARCHAR(255) DEFAULT '',
   producer VARCHAR(255) DEFAULT '',
   priority INT(10) NOT NULL DEFAULT '0',
   runcount INT(10) NOT NULL DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1416970)',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME
 );
 
+--
+-- Table: testrun_requested_feature
+--
+DROP TABLE testrun_requested_feature;
+
+CREATE TABLE testrun_requested_feature (
+  id INTEGER PRIMARY KEY NOT NULL,
+  testrun_id INT(11) NOT NULL,
+  feature VARCHAR(255) DEFAULT ''
+);
+
+CREATE INDEX testrun_requested_feature_idx_testrun_id ON testrun_requested_feature (testrun_id);
+
+--
+-- Table: preconditiontype
+--
+DROP TABLE preconditiontype;
+
+CREATE TABLE preconditiontype (
+  name VARCHAR(20) NOT NULL,
+  description TEXT NOT NULL DEFAULT '',
+  PRIMARY KEY (name)
+);
+
+--
+-- Table: testrun_scheduling
+--
+DROP TABLE testrun_scheduling;
+
+CREATE TABLE testrun_scheduling (
+  id INT NOT NULL,
+  testrun_id INTEGER PRIMARY KEY NOT NULL,
+  queue_id INT(11) DEFAULT '0',
+  built INT(1) DEFAULT '0',
+  active INT(1) DEFAULT '0',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME
+);
+
+CREATE INDEX testrun_scheduling_idx_queue_id ON testrun_scheduling (queue_id);
+
+CREATE INDEX testrun_scheduling_idx_testrun_id ON testrun_scheduling (testrun_id);
 
 --
 -- Table: topic
 --
 DROP TABLE topic;
+
 CREATE TABLE topic (
   name VARCHAR(20) NOT NULL,
   description TEXT NOT NULL DEFAULT '',
   PRIMARY KEY (name)
 );
 
-
 --
 -- Table: user
 --
 DROP TABLE user;
+
 CREATE TABLE user (
   id INTEGER PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -65,11 +99,11 @@ CREATE TABLE user (
   password VARCHAR(255)
 );
 
-
 --
 -- Table: pre_precondition
 --
 DROP TABLE pre_precondition;
+
 CREATE TABLE pre_precondition (
   parent_precondition_id INT(11) NOT NULL,
   child_precondition_id INT(11) NOT NULL,
@@ -77,13 +111,15 @@ CREATE TABLE pre_precondition (
   PRIMARY KEY (parent_precondition_id, child_precondition_id)
 );
 
-CREATE INDEX pre_precondition_idx_child_precondition_id_pre ON pre_precondition (child_precondition_id);
-CREATE INDEX pre_precondition_idx_parent_precondition_id_pr ON pre_precondition (parent_precondition_id);
+CREATE INDEX pre_precondition_idx_child_precondition_id ON pre_precondition (child_precondition_id);
+
+CREATE INDEX pre_precondition_idx_parent_precondition_id ON pre_precondition (parent_precondition_id);
 
 --
 -- Table: testrun
 --
 DROP TABLE testrun;
+
 CREATE TABLE testrun (
   id INTEGER PRIMARY KEY NOT NULL,
   shortname VARCHAR(255) DEFAULT '',
@@ -97,29 +133,19 @@ CREATE TABLE testrun (
   owner_user_id INT(11),
   test_program VARCHAR(255) NOT NULL DEFAULT '',
   wait_after_tests INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1438370)',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME
 );
 
-CREATE INDEX testrun_idx_owner_user_id_test ON testrun (owner_user_id);
-CREATE INDEX testrun_idx_topic_name_testrun ON testrun (topic_name);
+CREATE INDEX testrun_idx_owner_user_id ON testrun (owner_user_id);
 
---
--- Table: testrun_requested_feature
---
-DROP TABLE testrun_requested_feature;
-CREATE TABLE testrun_requested_feature (
-  id INTEGER PRIMARY KEY NOT NULL,
-  testrun_id INT(11) NOT NULL,
-  feature VARCHAR(255) DEFAULT ''
-);
-
-CREATE INDEX testrun_requested_feature_idx_testrun_id_testrun_reques ON testrun_requested_feature (testrun_id);
+CREATE INDEX testrun_idx_topic_name ON testrun (topic_name);
 
 --
 -- Table: testrun_precondition
 --
 DROP TABLE testrun_precondition;
+
 CREATE TABLE testrun_precondition (
   testrun_id INT(11) NOT NULL,
   precondition_id INT(11) NOT NULL,
@@ -127,24 +153,8 @@ CREATE TABLE testrun_precondition (
   PRIMARY KEY (testrun_id, precondition_id)
 );
 
-CREATE INDEX testrun_precondition_idx_precondition_id_testrun_p ON testrun_precondition (precondition_id);
-CREATE INDEX testrun_precondition_idx_testrun_id_testrun_precon ON testrun_precondition (testrun_id);
+CREATE INDEX testrun_precondition_idx_precondition_id ON testrun_precondition (precondition_id);
 
---
--- Table: testrun_scheduling
---
-DROP TABLE testrun_scheduling;
-CREATE TABLE testrun_scheduling (
-  id INT NOT NULL,
-  testrun_id INTEGER PRIMARY KEY NOT NULL,
-  queue_id INT(11) DEFAULT '0',
-  built INT(1) DEFAULT '0',
-  active INT(1) DEFAULT '0',
-  created_at TIMESTAMP DEFAULT 'SCALAR(0x1392860)',
-  updated_at DATETIME
-);
-
-CREATE INDEX testrun_scheduling_idx_queue_id_testrun_scheduli ON testrun_scheduling (queue_id);
-CREATE INDEX testrun_scheduling_idx_testrun_id_testrun_schedu ON testrun_scheduling (testrun_id);
+CREATE INDEX testrun_precondition_idx_testrun_id ON testrun_precondition (testrun_id);
 
 COMMIT;
