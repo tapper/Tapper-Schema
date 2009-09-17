562732988aec69fdb8d9f778c37dadaa44952439
Maik Hentsche <maik.hentsche@amd.com>
2009-09-17 16:29:37 +0200

    Schema: only active hosts are free

--- lib/Artemis/Schema/TestrunDB/ResultSet/Host.pm
+++ lib/Artemis/Schema/TestrunDB/ResultSet/Host.pm
@@ -7,6 +7,6 @@ use warnings;
 use parent 'DBIx::Class::ResultSet';
 use Data::Dumper;
 
-sub free_hosts { shift->search({ free => 1 }) }
+sub free_hosts { shift->search({ free => 1, active => 1 }) }
 
 1;
