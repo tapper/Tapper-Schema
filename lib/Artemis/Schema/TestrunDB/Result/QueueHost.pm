d46b90b6c8842112205833e6ff35384b7a9e646a
Maik Hentsche <maik.hentsche@amd.com>
2009-09-22 13:35:11 +0200

    Schema: added QueueHost table to allow queuebound hosts

---
+++ lib/Artemis/Schema/TestrunDB/Result/QueueHost.pm
@@ -0,0 +1,53 @@
+package Artemis::Schema::TestrunDB::Result::QueueHost;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class';
+
+__PACKAGE__->load_components("Core");
+__PACKAGE__->table("queue_host");
+__PACKAGE__->add_columns
+    (
+     "id",              { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
+     "queue_id",        { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key    => 1, },
+     "host_id",         { data_type => "INT", default_value => undef, is_nullable => 1,                                     },
+    );
+
+__PACKAGE__->set_primary_key(qw/id/);
+
+(my $basepkg = __PACKAGE__) =~ s/::\w+$//;
+
+__PACKAGE__->belongs_to( queue             => "${basepkg}::Testrun", { 'foreign.id' => 'self.queue_id' });
+__PACKAGE__->belongs_to( host              => "${basepkg}::Host",    { 'foreign.id' => 'self.host_id'  });
+
+1;
+
+=head1 NAME
+
+Artemis::Schema::TestrunDB::Result::PrePrecondition - A ResultSet description
+
+
+=head1 SYNOPSIS
+
+Abstraction for the database table.
+
+ use Artemis::Schema::TestrunDB;
+
+
+=head1 AUTHOR
+
+OSRC SysInt Team, C<< <osrc-sysint at elbe.amd.com> >>
+
+
+=head1 BUGS
+
+None.
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 OSRC SysInt Team, all rights reserved.
+
+This program is released under the following license: restrictive
+
