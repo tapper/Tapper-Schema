43d0740943d5f02d48fe7f225474b43015ba96d8
Maik Hentsche <maik.hentsche@amd.com>
2009-10-15 10:11:12 +0200

    Schema: new update scripts to version 2.010023

---
+++ upgrades/Artemis-Schema-TestrunDB-2.010021-2.010023-SQLite.sql
@@ -0,0 +1,9 @@
+-- Convert schema 'upgrades/Artemis-Schema-TestrunDB-2.010021-SQLite.sql' to 'upgrades/Artemis-Schema-TestrunDB-2.010023-SQLite.sql':;
+
+BEGIN;
+
+DROP INDEX testrun_idx_topic_name;
+
+
+COMMIT;
+
