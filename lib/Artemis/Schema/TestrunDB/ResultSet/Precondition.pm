cb74da2b53a942f1dfcc57dd6d3153927038cba4
Maik Hentsche <maik.hentsche@amd.com>
2009-11-23 14:20:18 +0100

    Schema: always expect preconditions as array ref, put yaml handling into Cmd

--- lib/Artemis/Schema/TestrunDB/ResultSet/Precondition.pm
+++ lib/Artemis/Schema/TestrunDB/ResultSet/Precondition.pm
@@ -4,18 +4,13 @@ use strict;
 use warnings;
 
 use parent 'DBIx::Class::ResultSet';
-use Artemis::Exception::Param;
 use YAML::Syck;
 
 
 sub add {
-        my ($self, $yaml) = @_;
+        my ($self, $preconditions) = @_;
 
-        $yaml .= "\n" unless $yaml =~ /\n$/;
-        my $yaml_error = Artemis::Schema::TestrunDB::_yaml_ok($yaml);
-        die Artemis::Exception::Param->new($yaml_error) if $yaml_error;
-
-        my @precond_list = Load($yaml);
+        my @precond_list = @$preconditions;
         my @precond_ids;
 
         foreach my $precond_data (@precond_list) {
