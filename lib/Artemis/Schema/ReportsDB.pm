635c152cdc363175add208869e02645696d7ed10
Steffen Schwigon <steffen.schwigon@amd.com>
2010-05-17 14:36:30 +0200

    Schema: add fields for simnow metainfo headers

--- lib/Artemis/Schema/ReportsDB.pm
+++ lib/Artemis/Schema/ReportsDB.pm
@@ -7,7 +7,7 @@ use warnings;
 
 # Only increment this version here on schema changes.
 # For everything else increment Artemis/Schema.pm.
-our $VERSION = '2.010030';
+our $VERSION = '2.010031';
 
 # avoid these warnings
 #   Subroutine initialize redefined at /2home/ss5/perl510/lib/site_perl/5.10.0/Class/C3.pm line 70.
