0d0fce3bee7481c514fc7244b1daaf5d9159d339
Maik Hentsche <maik.hentsche@amd.com>
2010-05-31 09:35:51 +0200

    MCP: added "has_ecc" as requestable feature

--- lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
+++ lib/Artemis/Schema/TestrunDB/Result/TestrunScheduling.pm
@@ -6,6 +6,8 @@ use 5.010;
 use strict;
 use warnings;
 use YAML::Syck;
+use Perl6::Junction qw/ any /;
+
 use parent 'DBIx::Class';
 
 __PACKAGE__->load_components("InflateColumn::Object::Enum", "Core");
@@ -89,18 +91,19 @@ sub _helper {
 # vendor eq "AMD";      # without argument returns the value
 # @_ means this optional param
 # $_ is the current context inside the while-loop (see below) where the eval happens
-sub hostname(;$) { _helper($_->{features}{hostname}, undef,      @_) }
-sub mem(;$)      { _helper($_->{features}{mem},      undef,      @_) }
-sub vendor(;$)   { _helper($_->{features}{cpu},      'vendors',  @_) }
-sub family(;$)   { _helper($_->{features}{cpu},      'family',   @_) }
-sub model(;$)    { _helper($_->{features}{cpu},      'model',    @_) }
-sub stepping(;$) { _helper($_->{features}{cpu},      'stepping', @_) }
-sub revision(;$) { _helper($_->{features}{cpu},      'revision', @_) }
-sub socket(;$)   { _helper($_->{features}{cpu},      'socket',   @_) }
-sub cores(;$)    { _helper($_->{features}{cpu},      'cores',    @_) }
-sub clock(;$)    { _helper($_->{features}{cpu},      'clock',    @_) }
-sub l2cache(;$)  { _helper($_->{features}{cpu},      'l2cache',  @_) }
-sub l3cache(;$)  { _helper($_->{features}{cpu},      'l3cache',  @_) }
+sub hostname(;$)    { _helper($_->{features}{hostname}, undef,      @_) }
+sub mem(;$)         { _helper($_->{features}{mem},      undef,      @_) }
+sub vendor(;$)      { _helper($_->{features}{cpu},      'vendors',  @_) }
+sub family(;$)      { _helper($_->{features}{cpu},      'family',   @_) }
+sub model(;$)       { _helper($_->{features}{cpu},      'model',    @_) }
+sub stepping(;$)    { _helper($_->{features}{cpu},      'stepping', @_) }
+sub revision(;$)    { _helper($_->{features}{cpu},      'revision', @_) }
+sub socket_type(;$) { _helper($_->{features}{cpu},      'socket',   @_) }
+sub cores(;$)       { _helper($_->{features}{cpu},      'cores',    @_) }
+sub clock(;$)       { _helper($_->{features}{cpu},      'clock',    @_) }
+sub l2cache(;$)     { _helper($_->{features}{cpu},      'l2cache',  @_) }
+sub l3cache(;$)     { _helper($_->{features}{cpu},      'l3cache',  @_) }
+sub has_ecc()       { socket_type($_) eq any('F', 'C32', 'G34') ? 1 : 0 }
 
 sub match_feature {
         my ($self, $free_hosts) = @_;
