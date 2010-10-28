51efad3661e9f60edfabb0de14802378a336649b
Maik Hentsche <maik.hentsche@amd.com>
2010-10-28 12:04:38 +0200

    Schema: [FIX] renamed key field to entry Key is a SQL keyword. Need a different field name in HostFeature

--- lib/Artemis/Schema/TestrunDB/Result/HostFeature.pm
+++ lib/Artemis/Schema/TestrunDB/Result/HostFeature.pm
@@ -10,12 +10,13 @@ __PACKAGE__->load_components(qw/InflateColumn::DateTime Core/);
 __PACKAGE__->table("host_feature");
 __PACKAGE__->add_columns
     (
-     "id",                        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11,    is_auto_increment => 1, },
-     "host_id",                   { data_type => "VARCHAR",   default_value => "",                   is_nullable => 1, size => 255,                           },
-     "key",                      { data_type => "TINYINT",   default_value => "0",                  is_nullable => 1,                                        },
-     "value",                    { data_type => "TINYINT",   default_value => "0",                  is_nullable => 1,                                        },
-     "created_at",                { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                        }, # '
-     "updated_at",                { data_type => "DATETIME",  default_value => undef,                is_nullable => 1,                                        },
+     "id",         { data_type => "INT",       default_value => undef,                is_nullable => 0, size =>  11, is_auto_increment => 1, },
+     "host_id",    { data_type => "INT",       default_value => undef,                is_nullable => 0,                                      },
+     # can't call it key because that's a SQL keyword
+     "entry",        { data_type => "VARCHAR",   default_value => undef,                is_nullable => 0, size => 255,                       },
+     "value",      { data_type => "VARCHAR",   default_value => undef,                is_nullable => 0, size => 255,                         },
+     "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1,                                      },
+     "updated_at", { data_type => "DATETIME",  default_value => undef,                is_nullable => 1,                                      },
 
     );
 
@@ -23,7 +24,7 @@ __PACKAGE__->set_primary_key("id");
 
 (my $basepkg = __PACKAGE__) =~ s/::\w+$//;
 
-__PACKAGE__->belongs_to ( host => "${basepkg}::Host", { 'foreign.id' => 'self.host_id' });
+__PACKAGE__->belongs_to( host => "${basepkg}::Host", { 'foreign.id' => 'self.host_id'  });
 
 
 1;
