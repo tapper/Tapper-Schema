38dccf8594434a38f97e5719d49e69d38dfb7ff9
Steffen Schwigon <steffen.schwigon@amd.com>
2010-12-07 15:53:33 +0100

    Schema: [FIX] upgrade script had FK and INDEX failures

--- upgrades/Artemis-Schema-ReportsDB-2.010033-2.010034-MySQL.sql
+++ upgrades/Artemis-Schema-ReportsDB-2.010033-2.010034-MySQL.sql
@@ -2,17 +2,7 @@
 
 BEGIN;
 
-ALTER TABLE report DROP FOREIGN KEY report_fk_id,
-                   DROP FOREIGN KEY report_fk_id_1,
-                   DROP INDEX id,
-                   DROP COLUMN hardwaredb_systems_id;
-
-ALTER TABLE reportgrouparbitrary ADD INDEX reportgrouparbitrary_idx_report_id (report_id),
-                                 ADD CONSTRAINT reportgrouparbitrary_fk_report_id FOREIGN KEY (report_id) REFERENCES report (id) ON DELETE CASCADE;
-
-ALTER TABLE reportgrouptestrun ADD INDEX reportgrouptestrun_idx_report_id (report_id),
-                               ADD CONSTRAINT reportgrouptestrun_fk_report_id FOREIGN KEY (report_id) REFERENCES report (id) ON DELETE CASCADE;
-
+ALTER TABLE report DROP COLUMN hardwaredb_systems_id;
 
 COMMIT;
 
