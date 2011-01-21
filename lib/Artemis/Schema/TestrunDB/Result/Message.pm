07094a5ed09f814c88dbb3dcd2d2dc3d0d5ec796
Maik Hentsche <maik.hentsche@amd.com>
2011-01-21 11:14:26 +0100

    Schema: automatic converting of messages
    Messages are stored as YAML but returned as Perl data structure. The
    result class handles the transformation transparently.

--- lib/Artemis/Schema/TestrunDB/Result/Message.pm
+++ lib/Artemis/Schema/TestrunDB/Result/Message.pm
@@ -4,9 +4,10 @@ use strict;
 use warnings;
 
 use parent 'DBIx::Class';
+use YAML::XS;
 
-__PACKAGE__->load_components(qw/InflateColumn::DateTime Core/);
-__PACKAGE__->table("state");
+__PACKAGE__->load_components(qw/InflateColumn::DateTime Core InflateColumn/);
+__PACKAGE__->table("message");
 __PACKAGE__->add_columns
     ( "id",        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_foreign_key    => 1, },
@@ -14,12 +15,14 @@ __PACKAGE__->add_columns
      "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1, },
      "updated_at", { data_type => "DATETIME",  default_value => undef,                is_nullable => 1, },
     );
-
+__PACKAGE__->inflate_column( message => {
+                                        inflate => sub { YAML::XS::Load(shift) },
+                                        deflate => sub { YAML::XS::Dump(shift)},
+ });
 
 (my $basepkg = __PACKAGE__) =~ s/::\w+$//;
 
 __PACKAGE__->set_primary_key("id");
-__PACKAGE__->add_unique_constraint( unique_testrun_id => [ qw/testrun_id/ ], );
 __PACKAGE__->belongs_to( testrun => "${basepkg}::Testrun", { 'foreign.id' => 'self.testrun_id' });
 
 =head1 NAME
