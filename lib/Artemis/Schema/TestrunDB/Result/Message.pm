4bd5a86e4340d4ad739e7510725382a12568d473
Maik Hentsche <maik.hentsche@amd.com>
2011-01-26 15:16:56 +0100

    Schema: [FIX] varchar is only allowed with size

--- lib/Artemis/Schema/TestrunDB/Result/Message.pm
+++ lib/Artemis/Schema/TestrunDB/Result/Message.pm
@@ -11,7 +11,7 @@ __PACKAGE__->table("message");
 __PACKAGE__->add_columns
     ( "id",        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_foreign_key    => 1, },
-     "message",    { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, },
+     "message",    { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, size => 65000, },
      "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1, },
      "updated_at", { data_type => "DATETIME",  default_value => undef,                is_nullable => 1, },
     );
--- lib/Artemis/Schema/TestrunDB/Result/State.pm
+++ lib/Artemis/Schema/TestrunDB/Result/State.pm
@@ -10,7 +10,7 @@ __PACKAGE__->table("state");
 __PACKAGE__->add_columns
     ( "id",        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_foreign_key    => 1, },
-     "state",      { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, },
+     "state",      { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, size => 65000, },
      "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1, },
      "updated_at", { data_type => "DATETIME",  default_value => undef,                is_nullable => 1, },
     );
