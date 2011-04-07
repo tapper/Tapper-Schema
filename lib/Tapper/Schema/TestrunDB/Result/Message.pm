191e6875eb3b256aae2ea8b7e065c780726059c3
Maik Hentsche <maik.hentsche@amd.com>
2011-04-07 15:52:03 +0200

    [FIX] added type of message to correct module

--- lib/Tapper/Schema/TestrunDB/Result/Message.pm
+++ lib/Tapper/Schema/TestrunDB/Result/Message.pm
@@ -12,6 +12,7 @@ __PACKAGE__->add_columns
     ( "id",        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_foreign_key    => 1, },
      "message",    { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, size => 65000, },
+     "type",       { data_type => "VARCHAR",   is_nullable => 1, size => 255, is_enum => 1, extra => { list => [qw(action state)] } },
      "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1, },
      "updated_at", { data_type => "DATETIME",  default_value => undef,                is_nullable => 1, },
     );
