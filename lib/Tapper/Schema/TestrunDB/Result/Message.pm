97b0b2596cfd398279a6b7c28c6c35cc505001a2
Maik Hentsche <maik.hentsche@amd.com>
2011-05-05 11:11:08 +0200

    [FIX] testrun/message relationship
    A message belongs to a testrun. This relation is optional, i.e. the
    related testrun may be undef.

--- lib/Tapper/Schema/TestrunDB/Result/Message.pm
+++ lib/Tapper/Schema/TestrunDB/Result/Message.pm
@@ -10,7 +10,7 @@ __PACKAGE__->load_components(qw/InflateColumn::DateTime Core InflateColumn/);
 __PACKAGE__->table("message");
 __PACKAGE__->add_columns
     ( "id",        { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_auto_increment => 1, },
-     "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 0, size => 11, is_foreign_key    => 1, },
+     "testrun_id", { data_type => "INT",       default_value => undef,                is_nullable => 1, size => 11, is_foreign_key    => 1, },
      "message",    { data_type => "VARCHAR",   default_value => undef,                is_nullable => 1, size => 65000, },
      "type",       { data_type => "VARCHAR",   is_nullable => 1, size => 255, is_enum => 1, extra => { list => [qw(action state)] } },
      "created_at", { data_type => "TIMESTAMP", default_value => \'CURRENT_TIMESTAMP', is_nullable => 1, },
@@ -24,7 +24,7 @@ __PACKAGE__->inflate_column( message => {
 (my $basepkg = __PACKAGE__) =~ s/::\w+$//;
 
 __PACKAGE__->set_primary_key("id");
-__PACKAGE__->might_have( testrun => "${basepkg}::Testrun", { 'foreign.id' => 'self.testrun_id' });
+__PACKAGE__->belongs_to( testrun => "${basepkg}::Testrun", { 'foreign.id' => 'self.testrun_id' });
 
 =head1 NAME
 
