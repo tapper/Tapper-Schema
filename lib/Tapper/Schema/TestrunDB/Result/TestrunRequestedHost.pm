a271190372c8d745c28166684a5a0328aa7ab6a1
Maik Hentsche <maik.hentsche@amd.com>
2011-07-08 14:14:58 +0200

    [FIX] set host_id "foreign_key"
    host_id is foreign key in QueueHost and TestrunRequestedHost.

--- lib/Tapper/Schema/TestrunDB/Result/TestrunRequestedHost.pm
+++ lib/Tapper/Schema/TestrunDB/Result/TestrunRequestedHost.pm
@@ -11,7 +11,7 @@ __PACKAGE__->add_columns
     (
      "id",              { data_type => "INT",     default_value => undef, is_nullable => 0, size => 11, is_auto_increment => 1, },
      "testrun_id",      { data_type => "INT",     default_value => undef, is_nullable => 0, size => 11, is_foreign_key    => 1, },
-     "host_id",         { data_type => "INT",     default_value => undef,    is_nullable => 1,                                     },
+     "host_id",         { data_type => "INT", default_value => undef, is_nullable => 0, size => 11, is_foreign_key    => 1, },
     );
 
 __PACKAGE__->set_primary_key(qw/id/);
