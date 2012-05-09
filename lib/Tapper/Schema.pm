b5bf23635095b5be75010bb1bbd8016f19046299
Steffen Schwigon <steffen.schwigon@amd.com>
2012-05-09 13:22:45 +0200

    Дист::Зилла Roundhouse kick

--- lib/Tapper/Schema.pm
+++ lib/Tapper/Schema.pm
@@ -1,41 +1,7 @@
-
 package Tapper::Schema;
+# ABSTRACT: Tapper - Database schemas for Tapper
 
 use warnings;
 use strict;
-
-our $VERSION = '3.000019';
-
-=head1 NAME
-
-Tapper::Schema - Tapper - Database schemas for Tapper
-
-=head1 SYNOPSIS
-
-    use Tapper::Schema;
-    my $foo = Tapper::Schema->new();
-    ...
-
-=head1 AUTHOR
-
-AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-tapper-schema at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tapper-Schema>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2008-2011 AMD OSRC Tapper Team, all rights reserved.
-
-This program is released under the following license: freebsd
-
-
-=cut
 
 1; # End of Tapper::Schema
--- t/00-load.t
+++ t/00-load.t
@@ -5,5 +5,3 @@ use Test::More tests => 1;
 BEGIN {
 	use_ok( 'Tapper::Schema' );
 }
-
-diag( "Testing Tapper::Schema $Tapper::Schema::VERSION, Perl $], $^X" );
