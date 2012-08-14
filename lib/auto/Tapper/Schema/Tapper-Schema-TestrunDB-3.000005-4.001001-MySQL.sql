6a268ebf80e6bead5690d9b9c909acf2008a9fb4
Maik Hentsche <maik.hentsche@amd.com>
2012-08-14 13:12:19 +0200

    Upgrade schema files for testrundb
    The mysql upgrade file for 3.000005 to 4.001001 has some manual
    polishing in it.

---
+++ lib/auto/Tapper/Schema/Tapper-Schema-TestrunDB-3.000005-4.001001-MySQL.sql
@@ -0,0 +1,34 @@
+-- Convert schema '/home/hmai/Projekte/Tapper/src/Tapper-Schema/lib/auto/Tapper/Schema/Tapper-Schema-TestrunDB-3.000005-MySQL.sql' to 'Tapper::Schema::TestrunDB v4.001001':;
+
+BEGIN;
+
+SET foreign_key_checks=0;
+
+CREATE TABLE owner (
+  id integer(11) NOT NULL auto_increment,
+  name VARCHAR(255),
+  login VARCHAR(255) NOT NULL,
+  password VARCHAR(255),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+
+ALTER TABLE message CHANGE COLUMN message message text,
+                    CHANGE COLUMN type type VARCHAR(255);
+
+ALTER TABLE state CHANGE COLUMN state state text;
+
+ALTER TABLE testrun DROP FOREIGN KEY testrun_fk_owner_user_id,
+                    DROP INDEX testrun_idx_owner_user_id,
+                    CHANGE COLUMN owner_user_id owner_id integer(11),
+                    ADD INDEX testrun_idx_owner_id (owner_id),
+                    ADD CONSTRAINT testrun_fk_owner_id FOREIGN KEY (owner_id) REFERENCES owner (id);
+
+ALTER TABLE testrun_scheduling CHANGE COLUMN status status VARCHAR(255) DEFAULT 'prepare';
+SET foreign_key_checks=1;
+
+DROP TABLE user;
+
+
+COMMIT;
+
