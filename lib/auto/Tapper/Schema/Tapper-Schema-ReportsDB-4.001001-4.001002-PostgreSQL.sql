ca5f6b6b7913cf164cdcc0ceb17aa51e94c98e03
Steffen Schwigon <steffen.schwigon@amd.com>
2012-10-16 15:47:04 +0200

    [FIX] handle VIEWs with PostgreSQL schema upgrade
    We cannot change table column types used in
    VIEWs, therefore we drop the view and recreate
    it after the schema upgrade. We use the VIEW
    definition from the same version, we want to
    upgrade to, obviously.

--- lib/auto/Tapper/Schema/Tapper-Schema-ReportsDB-4.001001-4.001002-PostgreSQL.sql
+++ lib/auto/Tapper/Schema/Tapper-Schema-ReportsDB-4.001001-4.001002-PostgreSQL.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
+--
+-- temporarily drop View: "view_testrun_overview"
+--
+DROP VIEW "view_testrun_overview";
+
 ALTER TABLE report ALTER COLUMN suite_version TYPE character varying(255);
 
 ALTER TABLE report ALTER COLUMN reportername TYPE character varying(255);
@@ -26,6 +31,9 @@ CREATE INDEX report_idx_created_at on report (created_at);
 
 CREATE INDEX reportsection_idx_report_id on reportsection (report_id);
 
+CREATE VIEW "view_testrun_overview" ( "vtor_primary_report_id", "vtor_rgt_testrun_id", "vtor_rgts_success_ratio", "report_id", "report_machine_name", "report_created_at", "report_suite_id", "report_suite_name" ) AS
+    select   vtor.primary_report_id  as vtor_primary_report_id        , vtor.rgt_testrun_id     as vtor_rgt_testrun_id        , vtor.rgts_success_ratio as vtor_rgts_success_ratio        , report.id               as report_id        , report.machine_name     as report_machine_name        , report.created_at       as report_created_at        , report.suite_id         as report_suite_id        , suite.name              as report_suite_name from view_testrun_overview_reports vtor,      report report,      suite suite where CAST(vtor.primary_report_id as INTEGER)=report.id and       report.suite_id=suite.id
+;
 
 COMMIT;
 
