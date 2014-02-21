248b5eed223a18e1256dd147f4e3f76a5a85675f
Roberto Schaefer <schaefr@amazon.com>
2014-02-21 10:53:31 +0100

    add missing "change column" for backup bench values

--- lib/auto/Tapper/Schema/Tapper-Schema-TestrunDB-4.001041-MySQL.sql
+++ lib/auto/Tapper/Schema/Tapper-Schema-TestrunDB-4.001041-MySQL.sql
@@ -4,3 +4,6 @@ use `testrundb`;
 ALTER TABLE `testrundb`.`bench_values`
     CHANGE COLUMN `bench_value` `bench_value` VARCHAR(512) NULL DEFAULT NULL COMMENT 'value for bench data point'
 ;
+ALTER TABLE `testrundb`.`bench_backup_values`
+    CHANGE COLUMN `bench_value` `bench_value` VARCHAR(512) NULL DEFAULT NULL COMMENT 'value for bench data point'
+;
