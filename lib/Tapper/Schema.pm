4723f203d0a074171bc2c6f704366103b1e9d36a
Neil Bowers <neil@bowers.com>
2015-02-26 21:02:36 +0000

    Doc improvements with SEE ALSO links

--- lib/Tapper/Schema.pm
+++ lib/Tapper/Schema.pm
@@ -5,3 +5,24 @@ use warnings;
 use strict;
 
 1; # End of Tapper::Schema
+
+=head1 DESCRIPTION
+
+This distribution contains the database schemas for 
+the L<Tapper> test framework.
+
+L<Tapper> is based on the Test Anything Protocol (TAP),
+which is the test I/O format used by all CPAN modules,
+driven by M<Test::Harness>.
+
+=head1 SEE ALSO
+
+L<Tapper> - the documentation for the Tapper framework.
+
+L<testanything.org|http://testanything.org> - the home page
+for the Test Anything Protocol (TAP).
+
+=head1 REPOSITORY
+
+L<https://github.com/tapper/Tapper-Schema>
+
